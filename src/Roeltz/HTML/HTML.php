4c4e49d473c9339f30113bf734da1de219c52871
Leonardo Rothe Tagliafico <leonardo.rothe@shiftactive.com>
2019-11-15 16:31:29 -0500

    All HTML methods should be static

--- src/Roeltz/HTML/HTML.php
+++ src/Roeltz/HTML/HTML.php
@@ -8,11 +8,11 @@ class HTML {
 		return new HTMLTag($method, ...$args);
 	}
 
-	function tag($type, array $attr = [], array $children = []) {
+	static function tag($type, array $attr = [], array $children = []) {
 		return new HTMLTag($type, $attr, $children);
 	}
 	
-	function defaultAttr($name, $value, array $extra = []) {
+	static function defaultAttr($name, $value, array $extra = []) {
 		$attr = is_array($value) ? $value : [$name=>$value];
 	
 		if ($extra)
@@ -21,26 +21,26 @@ class HTML {
 		return $attr;
 	}
 	
-	function a($href, $text, array $attr = []) {
+	static function a($href, $text, array $attr = []) {
 		$attr = array_merge(["href"=>$href], $attr);
 		return new HTMLTag("a", $attr, [$text]);
 	}
 	
-	function checkbox($attr = null, $value = null, $checked = false) {
+	static function checkbox($attr = null, $value = null, $checked = false) {
 		$attr = self::defaultAttr("name", $attr, ["type"=>"checkbox", "value"=>$value, "checked"=>$checked]);
 		return new HTMLTag("input", $attr);
 	}
 	
-	function input($type, $attr, $value = null) {
+	static function input($type, $attr, $value = null) {
 		$attr = self::defaultAttr("name", $attr, ["type"=>$type, "value"=>$value]);
 		return new HTMLTag("input", $attr);
 	}
 	
-	function img($src, $alt = null) {
+	static function img($src, $alt = null) {
 		return new HTMLTag("img", ["src"=>$src, "alt"=>$alt]);
 	}
 	
-	function label($text, $for = null) {
+	static function label($text, $for = null) {
 		if ($for instanceof HTMLTag) {
 			if ($id = @$for->attr["id"]) {
 				$for = $id;
@@ -54,22 +54,22 @@ class HTML {
 		return new HTMLTag("label", ["for"=>$for], [$text]);
 	}
 	
-	function p($children) {
+	static function p($children) {
 		if (is_string($children))
 			$children = [$children];
 		return new HTMLTag("p", [], $children);
 	}
 	
-	function pre($text) {
+	static function pre($text) {
 		return new HTMLTag("pre", [], [$text]);
 	}
 	
-	function radio($attr, $value = null, $checked = false) {
+	static function radio($attr, $value = null, $checked = false) {
 		$attr = self::defaultAttr("name", $attr, ["type"=>"radio", "value"=>$value, "checked"=>$checked]);
 		return new HTMLTag("input", $attr);
 	}
 	
-	function select($attr, array $options = [], $selectedValue = "") {
+	static function select($attr, array $options = [], $selectedValue = "") {
 		$attr = self::defaultAttr("name", $attr);
 		$select = new HTMLTag("select", $attr);
 	
@@ -83,7 +83,7 @@ class HTML {
 		return $select;
 	}
 	
-	function textarea($attr, $value) {
+	static function textarea($attr, $value) {
 		$attr = self::defaultAttr("name", $attr);
 		return new HTMLTag("textarea", $attr, [$value]);
 	}
